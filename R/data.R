bd8fb7a4245e699cc8eec3ce968a81c8c51ff68c
mjfii <mjfii@live.com>
2017-04-26 12:18:27 -0600

    updated the data documentation

---
+++ R/data.R
@@ -0,0 +1,39 @@
+#' Ayn Rand's The Fountainhead
+#'
+#' This R package the complete text of Ayn Rand's classic 1943 novel, The Fountainhead.  The book is broken
+#' down into 4 parts: Peter Keating, Ellsworth M. Toohey, Gail Wynard, and Howard Roark.  Each part of the book
+#' is represented as a data set in the package.  Each data set is a character vector where each element 
+#' represented by a chapter in the book.
+#' 
+#' @docType package
+#' @name fountainhead
+#' @aliases fountainhead
+NULL
+
+#' "Part One: Peter Keating"
+#'
+#' A charater vector of each chapter in Part One of The Fountainhead.
+#'
+#' @format character(15)
+"peter_keating"
+
+#' "Part Two: Ellsworth M Toohey"
+#'
+#' A charater vector of each chapter in Part Two of The Fountainhead.
+#'
+#' @format character(15)
+"ellsworth_m_toohey"
+
+#' "Part Three: Gail Wynard"
+#'
+#' A charater vector of each chapter in Part Three of The Fountainhead.
+#'
+#' @format character(15)
+"gail_wynand"
+
+#' "Part Four: Howard Roark"
+#'
+#' A charater vector of each chapter in Part Four of The Fountainhead.
+#'
+#' @format character(15)
+"howard_roark"
